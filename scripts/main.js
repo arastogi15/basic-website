6a873c8f853415bb65c784e93c9b629895552483
Ankur Rastogi <ankurras@usc.edu>
2018-03-14 00:32:01 -0700

    working on js

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+var myVariable = 'Bob';
+myVariable = "Steve";
+document.querySelector('html').onclick = function() {
+	alert('Ouch! Stop poking me!');
+}
